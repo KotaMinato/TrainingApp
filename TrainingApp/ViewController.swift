94f35af050e765173bf89f5288febd21aee8342f
kota0207 <minato.innetwork@gmail.com>
2021-03-08 12:58:13 +0900

    画面遷移完了

--- TrainingApp/ViewController.swift
+++ TrainingApp/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view.
+        let string: String
+                if let count = navigationController?.viewControllers.count {
+                    string = "\(count)"
+                } else {
+                    string = ""
+                }
+                title = string
     }
 
-
+    override func didReceiveMemoryWarning() {
+           super.didReceiveMemoryWarning()
+           // Dispose of any resources that can be recreated.
+       }
+    
+    
+    
+    @IBAction func pushButtonTapped(_ sender: Any) {
+        guard let viewControlelr = storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController else {
+                    return
+                }
+                navigationController?.pushViewController(viewControlelr, animated: true)
+    
+    
+    }
+    
+    
+    
 }
 
